be92bafefda2fea55997815909cd64c4fd4e9a44
kereme0102 <61414445+kereme0102@users.noreply.github.com>
2020-06-11 19:55:37 +0300

    pizza
    kadirgt

---
+++ pizza/Assets/NewBehaviourScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+
+      
+        public AudioClip[] sesDosyalari;  // ses dosyları için gerekli dizini oluşturduk
+        
+      
+     public float sağ; // pizzanın sağa gitmesini sağlayacak değişken
+
+    public float sol;  // pizzanın sola gitmesini sağlayacak değişken
+    // Start is called before the first frame update
+    void Start()
+    {
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+         if (Input.GetKeyDown(KeyCode.A))  // a tuşuna basılınca pizzanın sola hareket etmesini sağladık
+
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.left * sol);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.D))   // a tuşuna basılınca pizzanın sağa hareket etmesini sağladık
+
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector3.right * sağ);
+        }
+    }
+
+         void OnCollisionEnter2D(Collision2D temas)  // temas adında bir collision oluşturduk
+    {
+        if (temas.gameObject.tag == "puan")  // eğer puan tag'i olan bir nesneye çarpar ise
+        {
+                  GetComponent<AudioSource>().PlayOneShot(sesDosyalari[0],  1);  // ses dosyasındaki 0'ıncı elementi çalıştır
+                  Destroy(temas.gameObject); // ve çarptığı nesneyi yok et
+                
+        }
+
+       
+       
+       
+    }
+
+
+
+
+
+     
+       
+       
+       
+    }
+    
+
---
+++ pizza/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter2D(Collision2D temas) // temas adında ikinci bir collision
+
+    {
+        if (temas.gameObject.tag == "engel") // arka planın altında sağında ve solunda belirli spritelar var bunların nedeni pizzanın kamera arkaplandan çıkmaması bu sayede onlara çarpıyor
+        {
+                
+        }
+
+
+    }
+}
